15c5cbc34408082bb2b497f188af246975830742
BisheshKatwal <kbishesh2072@gmail.com>
2017-12-02 19:41:05 +0545

    printing birthday song for your friend

---
+++ Birthday_song_qn3.py
@@ -0,0 +1,14 @@
+def song(name):
+    default = "Happy Birthday to you"
+    print("{}".format(default))
+    print("{}".format(default))
+    print("{0} dear {1}".format(default, name))
+    print("{}".format(default))
+
+
+name= input("Enter the name of your friend: ")
+song(name)
+
+
+
+
