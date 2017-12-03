8ad616bad08970a37ac2cfc6b1048c1593006555
BisheshKatwal <kbishesh2072@gmail.com>
2017-12-03 15:34:52 +0545

    printing string multiple times

---
+++ multiple_string_q5.py
@@ -0,0 +1,6 @@
+def print_string(str,num):
+    print(str*num,end="")
+
+str=input("Enter the string to be printed: ")
+num=int(input("Enter how many times do you want to print: "))
+print_string(str,num)
